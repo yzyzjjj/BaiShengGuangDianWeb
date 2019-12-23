17285b8fb47bfc978a88371f1b64ac63542457fe
wll <1291761009@qq.com>
2019-12-23 14:58:21 +0800

    点检

---
+++ BaiShengGuangDianWeb/BaiShengGuangDianWeb/wwwroot/mjs/DeviceSpotCheck/SpotCheckPlan.js
@@ -0,0 +1,20 @@
+﻿function pageReady() {
+    getSpotPlan();
+}
+//获取点检计划
+function getSpotPlan() {
+    var opType = 600;
+    if (!checkPermission(opType)) {
+        layer.msg("没有权限");
+        return;
+    }
+    var data = {}
+    data.opType = opType;
+    ajaxPost("/Relay/Post", data, function(ret) {
+        if (ret.errno != 0) {
+            layer.msg(ret.errmsg);
+            return;
+        }
+        console.log(ret);
+    });
+}
