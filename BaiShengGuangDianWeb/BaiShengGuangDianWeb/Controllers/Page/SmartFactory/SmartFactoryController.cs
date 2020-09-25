8d28642d70e3467e58fb1283c187652fead6be21
yzyzjjj <1042773917@qq.com>
2020-09-25 11:35:13 +0800

    smart

---
+++ BaiShengGuangDianWeb/BaiShengGuangDianWeb/Controllers/Page/SmartFactory/SmartFactoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiShengGuangDianWeb.Controllers.Page.Warning
+{
+    public class SmartFactoryController : Controller
+    {
+        /// <summary>
+        /// 智慧工厂
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
