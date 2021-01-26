5aa1f84ef00000dacc252a9022b5525118066f7a
wanglulin <1291761009@qq.com>
2021-01-26 15:05:24 +0800

    test

--- BaiShengGuangDianWeb/BaiShengGuangDianWeb/wwwroot/mjs/Setting/uploadApiTest.js
+++ BaiShengGuangDianWeb/BaiShengGuangDianWeb/wwwroot/mjs/Setting/uploadApiTest.js
@@ -5,7 +5,6 @@ function pageReady() {
     $("#addFile").fileinput('clear');
 }
 
-
 function addFile() {
     if (!hasPre())
         return;
@@ -45,7 +44,51 @@ function addFile() {
     };
 
     var doSth = function () {
-        $('#addFile').fileinput("upload");
+        $('#addImage').fileinput("upload");
+    }
+    showConfirm("操作", doSth);
+}
+
+function addImage() {
+    if (!hasPre())
+        return;
+
+    fileCallBack[fileEnum.SpotCheck] = function (fileRet) {
+        console.log(fileRet);
+        if (fileRet.errno == 0) {
+            var imgs = [];
+            for (var key in fileRet.data) {
+                imgs.push(fileRet.data[key].newName);
+            }
+
+            //显示图片方法
+            var data = {
+                type: fileEnum.SpotCheck,
+                files: JSON.stringify(imgs)
+            };
+
+            ajaxPost("/Upload/Path", data,
+                function (ret) {
+                    if (ret.errno != 0) {
+                        layer.msg(ret.errmsg);
+                        return;
+                    }
+
+                    console.log(ret);
+
+                    $("#showImg").empty();
+                    var img = '<img height="200px" src="{0}"/>';
+                    var html = "";
+                    for (var i = 0; i < ret.data.length; i++) {
+                        html += img.format(ret.data[i].path);
+                    }
+                    $("#showImg").append(html);
+                });
+        }
+    };
+
+    var doSth = function () {
+        $('#addImage').fileinput("upload");
     }
     showConfirm("操作", doSth);
 }
